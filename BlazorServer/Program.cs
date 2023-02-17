72147fed1105505afb741215d134bbdaffece5c7
bogse <ciucuras.bogdan@gmail.com>
2023-02-17 17:52:06 +0200

    Add Signal R with Blazor Server and WPF client

---
+++ BlazorServer/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServer.Hubs;
+
+namespace BlazorServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            
+            // Add server ability to process octet stream and its added compression
+            // Configured everything to properly talk to a webserver
+            builder.Services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });     
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+
+            app.MapHub<ChatHub>("/chathub");
+            app.MapHub<CounterHub>("/counterhub");
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
